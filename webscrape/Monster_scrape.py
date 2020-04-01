172eaf50218ff2e21f1d0c66b19950bff3aaaf85
frostyy831 <54964421+frostyy831@users.noreply.github.com>
2020-03-31 23:30:16 -0700

    Create Monster_scrape.py

---
+++ webscrape/Monster_scrape.py
@@ -0,0 +1,18 @@
+# Name: Fausto Preciado
+# Date: 3/26/20
+# Id number: 110588341
+# Description: Project for CSCI-152, job scrapper
+# Scrapping the monster job website.
+
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+import requests
+import pandas as pd
+
+#Enter the location on the command prompt.
+location = input("Enter the city to search:")
+
+webUrl = 'https://www.monster.com/jobs/search/?q=Computer-Science&where=' + location
+content = requests.get(webUrl)
+print(webUrl)
+
