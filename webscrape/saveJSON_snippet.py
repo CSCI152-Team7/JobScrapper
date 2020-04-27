1591296d0edd151556eb4b8220f643aa4e5d8e2d
schavarin21 <54963912+schavarin21@users.noreply.github.com>
2020-04-26 20:24:04 -0700

    Update saveJSON_snippet.py

--- webscrape/saveJSON_snippet.py
+++ webscrape/saveJSON_snippet.py
@@ -1,7 +1,20 @@
+import io
 import json
 
-r = df.to_json(orient='index')
-r=json.loads(r)
-# r=json.dumps(r, indent=4)
-with open('glassdoor.json', 'w') as fp:
-    json.dump(r,fp,indent=4)
+x={}
+for i in range(len(df)):
+    temp={}
+    temp["model"] = "jobs.jobsinfo"
+    temp["pk"]= i
+    x[i]=temp
+    x[i]["fields"]={
+        "jobTitle":(df["jobtitle"][i]),
+        "description": df["description"][i],
+        "place":df["place"][i],
+        "link":df["link"][i],
+        "site":df["site"][i]}
+x=list(x.values())
+    
+with io.open('test.json', 'w',encoding='windows-1252') as fp:
+    json.dump(x,fp)
+#Note: My django did not like indents so I got rid of them
